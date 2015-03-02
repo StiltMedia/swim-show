aee55a32c1ef4f700cb505309da674635ff76c37
Mark Natera <marknatera@gmail.com>
2015-03-02 17:01:47 -0500

    Removed all picture from app and created Restaurant model

---
+++ app/admin/restaurant.rb
@@ -0,0 +1,40 @@
+ActiveAdmin.register Restaurant do
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  permit_params :name, :address, :phone, :description, :location_id, :destroy, :create
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+
+  index do
+    selectable_column
+    column :name
+    column :address
+    column :phone
+    column :description
+    column :location
+    actions
+  end
+
+  form do |f|
+    f.inputs "Club Details" do
+      f.input :name
+      f.input :location, prompt: 'Select Location'
+      f.input :phone
+      f.input :address
+      f.input :description
+    end
+    f.actions
+  end
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227200759) do
+ActiveRecord::Schema.define(version: 20150302211351) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -53,6 +53,16 @@ ActiveRecord::Schema.define(version: 20150227200759) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "phone"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "location_id"
   end
 
 end
