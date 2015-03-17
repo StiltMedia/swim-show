229eee2f9c7a8546909f7d81e3e977e738eb4191
Mark Natera <marknatera@gmail.com>
2015-03-16 23:25:12 -0400

    Added Migration

---
+++ db/migrate/20150317032434_add_vendor_registration_to_product.rb
@@ -0,0 +1,5 @@
+class AddVendorRegistrationToProduct < ActiveRecord::Migration
+  def change
+    add_column :products, :vendor_registration_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316184451) do
+ActiveRecord::Schema.define(version: 20150317032434) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -123,8 +123,9 @@ ActiveRecord::Schema.define(version: 20150316184451) do
   create_table "products", force: :cascade do |t|
     t.string   "category"
     t.string   "price_point"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "vendor_registration_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
