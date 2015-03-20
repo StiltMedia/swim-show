0087c5ffea42aec855fc999c85e1adae34f61a11
Mark Natera <marknatera@gmail.com>
2015-03-20 15:54:13 -0400

    Fixed registration forms for now

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -2,17 +2,17 @@ class Mailer < ApplicationMailer
 
   def contact_form(contact)
     @contact = contact
-    mail :to => "info@stiltmedia.com", :from => "swimshow_contact_form@swimshow.com", :subject => "From Contact Form: #{@contact.subject}"
+    mail :to => "info@stiltmedia.com", :from => "SwimShow@swimshow.com", :subject => "From Contact Form: #{@contact.subject}"
   end
 
   def media_registration_form(media)
     @media = media
-    mail :to  => "info@stiltmedia.com", :from => "registration@swimshow.com", :subject => "Media Registration Form"
+    mail :to  => "info@stiltmedia.com", :from => "SwimShow@swimshow.com", :subject => "Media Registration 2015"
   end
 
   def vendor_registration_form(vendor)
     @vendor = vendor
-    mail :to  => "info@stiltmedia.com", :from => "vendor_registration_form@swimshow.com", :subject => "Vendor Registration Form"
+    mail :to  => "info@stiltmedia.com", :from => "SwimShow@swimshow.com", :subject => "Retailer Pre-Registration 2015"
   end
 
 end
