3247aa745897d7e5a51c4a64ca99e3a060d93594
Mark Natera <marknatera@gmail.com>
2015-03-20 12:03:15 -0400

    Testing vendor registration page

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -2,12 +2,17 @@ class Mailer < ApplicationMailer
 
   def contact_form(contact)
     @contact = contact
-    mail :to => "marknatera@gmail.com", :from => "do-not-reply@swimshow.com", :subject => "Test #{@contact.subject}"
+    mail :to => "info@stiltmedia.com", :from => "swimshow_contact_form@swimshow.com", :subject => "From Contact Form: #{@contact.subject}"
   end
 
   def media_registration_form(media)
     @media = media
-    mail :to  => "marknatera@gmail.com", :from => "registration@swimshow.com", :subject => "Media Registration Form"
+    mail :to  => "info@stiltmedia.com", :from => "registration@swimshow.com", :subject => "Media Registration Form"
+  end
+
+  def vendor_registration_form(vendor)
+    @vendor = vendor
+    mail :to  => "info@stiltmedia.com", :from => "vendor_registration_form@swimshow.com", :subject => "Vendor Registration Form"
   end
 
 end
--- app/models/vendor_registration.rb
+++ app/models/vendor_registration.rb
@@ -1,4 +1,13 @@
 class VendorRegistration < ActiveRecord::Base
   has_many :products, :dependent => :destroy
   accepts_nested_attributes_for :products, allow_destroy: true
+
+
+  after_create :media_mailer
+
+  def vendor_mailer
+    vendor = self
+    Mailer.vendor_registration_form(media).deliver_now
+  end
+
 end
