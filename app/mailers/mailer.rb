c8095d1fa38b9148e474e555118de714e2f48263
Mark Natera <marknatera@gmail.com>
2015-04-06 13:08:42 -0400

    Changed Mailer

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -7,12 +7,12 @@ class Mailer < ApplicationMailer
 
   def media_registration_form(media)
     @media = media
-    mail :to  => ["info@stiltmedia.com","judy@swimshow.com"], :from => "SwimShow@swimshow.com", :subject => "Media Registration 2015"
+    mail :to  => ["mark@stiltmedia.com","judy@swimshow.com"], :from => "SwimShow@swimshow.com", :subject => "Media Registration 2015 | #{@media.first_name} #{@media.last_name} | #{@media.publication}"
   end
 
   def vendor_registration_form(vendor)
     @vendor = vendor
-    mail :to  => ["mark@stiltmedia.com","judy@swimshow.com"], :from => "SwimShow@swimshow.com", :subject => "Retailer Pre-Registration 2015"
+    mail :to  => ["mark@stiltmedia.com, judy@swimshow.com"], :from => "SwimShow@swimshow.com", :subject => "Retailer 2015 Pre-Registration | #{@vendor.first_name} #{@vendor.last_name} | #{@vendor.company}"
   end
 
 end
