2c3277fb4181cb3afb9929839bf1cea1758078ae
Mark Natera <marknatera@gmail.com>
2015-04-29 11:59:31 -0400

    Added migration

---
+++ db/migrate/20150429155806_add_years_attended_show_to_vendor_registration.rb
@@ -0,0 +1,5 @@
+class AddYearsAttendedShowToVendorRegistration < ActiveRecord::Migration
+  def change
+    add_column :vendor_registrations, :years_attened_show, :interger,  default: ‘1’
+  end
+end
