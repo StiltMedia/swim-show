4f40be689ea7e00113189e33b4c54b9d55738930
Mark Natera <marknatera@gmail.com>
2015-04-29 11:24:29 -0400

    Removed AddYearsAttendedShowToVendorRegistration

--- db/migrate/20150428223407_add_years_attended_show_to_vendor_registration.rb
+++
@@ -1,5 +0,0 @@
-class AddYearsAttendedShowToVendorRegistration < ActiveRecord::Migration
-  def change
-    add_column :vendor_registrations, :years_attended_show, :integer, default: ‘1’
-  end
-end
