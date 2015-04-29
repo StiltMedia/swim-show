58888036f85ac80b2f955959b5b66c57751a00c3
Anuvis Mejia <ameji012@gmail.com>
2015-04-29 12:08:58 -0400

    syntax for mig

--- db/migrate/20150429155806_add_years_attended_show_to_vendor_registration.rb
+++ db/migrate/20150429155806_add_years_attended_show_to_vendor_registration.rb
@@ -1,5 +1,5 @@
 class AddYearsAttendedShowToVendorRegistration < ActiveRecord::Migration
   def change
-    add_column :vendor_registrations, :years_attened_show, :interger,  default: 1
+    add_column :vendor_registrations, :years_attened_show, :integer,  default: 1
   end
 end
