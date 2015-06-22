49ef2cd6ccc537ce305eebc489bfc44d67597c9a
Mark Natera <marknatera@gmail.com>
2015-06-22 15:47:14 -0400

    Spacing in current lines

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-# require 'csv'
-#
-# CSV.foreach("db/2015-current-lines.csv") do |row|
-#     Line.create letter: row[0], name: row[1]
-# end
+require 'csv'
+
+CSV.foreach("db/2015-current-lines.csv") do |row|
+    Line.create letter: row[0], name: row[1]
+end
 
 # CSV.foreach("db/2014-west-coast-lines-list.csv") do |row|
 #     WestCoastLine.create letter: row[0], name: row[1]
